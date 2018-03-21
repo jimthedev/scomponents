7b666c3c8937412dd377f0aaf30dde0cdad21b34
Jim Cummins <jim.for.cy@gmail.com>
2018-03-21 09:59:43 -0500

    fix: add pre component

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ const styles = {
   pre: { backgroundColor: '#dedede', padding: '20px', textAlign: 'left', color: '#222', fontSize: '13px', marginBottom: '40px', fontWeight: '500' }
 };
 
+const Pre = ({children, ...props}) => (
+  <pre style={styles.pre} {...props}>{children}</pre>
+);
+
 class App extends React.Component {
   state = {
     alertType: "success",
@@ -19,26 +23,26 @@ class App extends React.Component {
         <H2>Alerts</H2>
         <H3>Basic</H3>
         <Alert type="tip" message="Tip!" />
-        <pre style={styles.pre}>{`<Alert type="tip" message="Tip!" />`}</pre>
+        <Pre>{`<Alert type="tip" message="Tip!" />`}</Pre>
         <Alert type="success" message="Success!" />
-        <pre style={styles.pre}>{`<Alert type="success" message="Success!" />`}</pre>
+        <Pre>{`<Alert type="success" message="Success!" />`}</Pre>
         <Alert type="error" message="Error!"  />
-        <pre style={styles.pre}>{`<Alert type="error" message="Error!" />`}</pre>
+        <Pre>{`<Alert type="error" message="Error!" />`}</Pre>
         <Alert type="warning" message="Warning!" />
-        <pre style={styles.pre}>{`<Alert type="warning" message="Warning!" />`}</pre>
+        <Pre>{`<Alert type="warning" message="Warning!" />`}</Pre>
         <Alert type="info" message="Info!" />
-        <pre style={styles.pre}>{`<Alert type="info" message="Info!" />`}</pre>
+        <Pre>{`<Alert type="info" message="Info!" />`}</Pre>
         <H3>Using render props</H3>
         <Alert type="tip" message={({ AlertMessage }) => (
           <AlertMessage type="tip" style={{ fontWeight: 'bold', color: 'purple', paddingLeft: '10px' }}>
             I am a customized alert message, while the surrounding alert box is kept the same but ultimately the text for this alert is also very long, excessively long.  It is as though someone did this on purpose.
           </AlertMessage>
         )} />
-        <pre style={{ textAlign: 'left', paddingTop: '2px' }}>{`<Alert type="tip" message={({ AlertMessage }) => (
+        <Pre>{`<Alert type="tip" message={({ AlertMessage }) => (
   <AlertMessage type="tip" style={{ fontWeight: 'bold', color: 'purple' }}>
     I am a customized alert message, while the box is kept the same.
   </AlertMessage>
-)} />`}</pre>
+)} />`}</Pre>
         
         <hr />
         <H2>Pick</H2>
